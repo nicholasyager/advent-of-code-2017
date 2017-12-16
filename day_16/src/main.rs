83d13a6c566f78ee26b095209658d134aa68b79c
Nicholas A. Yager <yager@nicholasyager.com>
2017-12-16 17:35:56 +0000

    Day 16: Part Two

--- day_16/src/main.rs
+++ day_16/src/main.rs
@@ -73,49 +73,64 @@ mod tests {
     }
 }
 
+fn vector_to_string(vector: Vec<char>) -> String {
+    vector.iter().fold(String::from(""), |vec_string, &c| {
+                let mut vec_clone = vec_string.clone();
+                vec_clone.push(c);
+                vec_clone
+            })
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename: String = args.get(1).unwrap().clone();
+    let dances: usize = args.get(2).unwrap().parse().unwrap();
 
     let mut file = String::new();
     let mut f = File::open(filename).expect("file not found");
     f.read_to_string(&mut file).expect("something went wrong reading the file");
 
-    let commands: Vec<String> = file.trim().split(",").map(|s| s.to_string()).collect();
-
+    let commands: Vec<String> = file.trim().split(",").map(|s| {s.to_string()}).collect();
 
     let mut vector: Vec<char> = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
                                      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];   
 
     println!("{:?}", vector);
 
-    for command_string in commands {
-        let new_vector = match command_string.chars().nth(0).unwrap() {
-            's' => {
-                let length: usize = command_string[1..].parse().unwrap();
-                spin(&vector, length)
-            }
-            'x' => {
-                let parts: Vec<&str> = command_string[1..].split("/").collect();
-                let coordinates: Vec<usize> = parts.iter().map(|c| String::from(*c).parse().unwrap()).collect();
-                exchange(&vector, coordinates[0], coordinates[1])
-            },
-            'p' => {
-                let parts: Vec<&str> = command_string[1..].split("/").collect();
-                let chars: Vec<char> = parts.iter().map(|c| String::from(*c).parse().unwrap()).collect();
-                partner(&vector, chars[0], chars[1])
-            },
-            _ => {vector.clone()}
-        };
+    let mut dances_seen: Vec<Vec<char>> = Vec::new();
 
-        vector = new_vector;
+    for dance_number in 0..dances {
 
-        let vector_string = vector.iter().fold(String::from(""), |vec_string, &c| {
-            let mut vec_clone = vec_string.clone();
-            vec_clone.push(c);
-            vec_clone
-        });
-        println!("{:?}", vector_string);
+        // Check for cycles. If we've seen this before, then compute the remainder.
+        if dances_seen.contains(&vector) {
+            println!("{:?}", vector_to_string(dances_seen[dances % dance_number].clone()));
+            return
+        } else {
+            dances_seen.push(vector.clone());
+        }
 
+        for command_string in &commands {
+            let new_vector = match command_string.chars().nth(0).unwrap() {
+                's' => {
+                    let length: usize = command_string[1..].parse().unwrap();
+                    spin(&vector, length)
+                }
+                'x' => {
+                    let parts: Vec<&str> = command_string[1..].split("/").collect();
+                    let coordinates: Vec<usize> = parts.iter().map(|c| String::from(*c).parse().unwrap()).collect();
+                    exchange(&vector, coordinates[0], coordinates[1])
+                },
+                'p' => {
+                    let parts: Vec<&str> = command_string[1..].split("/").collect();
+                    let chars: Vec<char> = parts.iter().map(|c| String::from(*c).parse().unwrap()).collect();
+                    partner(&vector, chars[0], chars[1])
+                },
+                _ => {vector.clone()}
+            };
+            vector = new_vector;  
+        }
+        let vector_string = vector_to_string(vector.clone());
+        println!("{:20} - {:?}", dance_number, vector_string);    
     }
+   
 }
