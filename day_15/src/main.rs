e773f4f39c797c0fe3a58a5d82b371cff12884aa
Nicholas A. Yager <yager@nicholasyager.com>
2017-12-16 01:03:21 +0000

    Day 15: Part 2

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -2,12 +2,20 @@ use std::env;
 use std::iter::Iterator;
 
 struct Generator {
-	multiplication_factor: u64
+	multiplication_factor: u64,
+	common_denominator: u64
 }
 
 impl Generator {
 	fn generate(&self, previous_value: u64) -> u64 {
-		(previous_value * self.multiplication_factor) % 2147483647_u64
+		let mut computing_value = previous_value;
+		loop {
+			let generated_value = (computing_value * self.multiplication_factor) % 2147483647_u64;
+			if generated_value % self.common_denominator == 0 {
+				return generated_value
+			}
+			computing_value = generated_value
+		}
 	}
 }
 
@@ -17,8 +25,8 @@ mod tests {
 
     #[test]
     fn test_generation() {
-    	let generator = Generator{multiplication_factor: 16807};
-    	assert_eq!(1092455, generator.generate(65));
+    	let generator = Generator{multiplication_factor: 16807, common_denominator: 4};
+    	assert_eq!(1352636452, generator.generate(65));
     }
 }
 
@@ -30,16 +38,16 @@ fn main() {
 
     println!("Generator A: {}, Generator B: {}.", generator_a_value, generator_b_value);
 
-    let generator_a = Generator{multiplication_factor: 16807};
-    let generator_b = Generator{multiplication_factor: 48271};
+    let generator_a = Generator{multiplication_factor: 16807, common_denominator: 4};
+    let generator_b = Generator{multiplication_factor: 48271, common_denominator: 8};
 
     let mut matches = 0;
 
-    for cycle in 0..cycles {
+    for _ in 0..cycles {
     	generator_a_value = generator_a.generate(generator_a_value);
     	generator_b_value = generator_b.generate(generator_b_value);
 
-    	if ((generator_a_value & 0xFFFF) == (generator_b_value & 0xFFFF)) {
+    	if (generator_a_value & 0xFFFF) == (generator_b_value & 0xFFFF) {
     		matches +=1 ;
     	}
 
