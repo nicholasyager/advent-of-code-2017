3cf65f4a3ba2f47384aaa3ecdf1f7e8888a33263
Nicholas A. Yager <yager@nicholasyager.com>
2018-01-04 02:36:20 +0000

    Day 18: Part Two

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -4,20 +4,27 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::collections::HashMap;
 
+#[derive(Debug, Clone)]
 struct Program {
-    send_registers: HashMap<String, i64>,
-    receive_registers: HashMap<String, Vec<i64>>,
-    data_registers: HashMap<String, i64>,
+    active: bool,
+    index: usize,
+    output: Option<i64>,
+    input_queue: Vec<i64>,
+    registers: HashMap<String, i64>,
+    recovered_sounds: u32,
+    sent_sounds: u32
 }
 
 impl Program {
     fn new() -> Program {
         Program{
-            send_registers: HashMap::new(),
-            receive_registers: HashMap::new(),
-            data_registers: HashMap::new(),
+            active: true,
             index: 0_usize,
-
+            output: None,
+            input_queue: Vec::new(),
+            registers: HashMap::new(),
+            recovered_sounds: 0_u32,
+            sent_sounds: 0_u32
         }
     }
 
@@ -47,60 +54,87 @@ impl Program {
         (command, parameter1, parameter2)
     }
 
-    fn process_command(&self, command: String) -> Program {
+    fn process_command(&mut self, raw_command: String) {
         
-        let mut new_program = self.clone();
+        //let mut new_program = self.clone();
+        let mut skip = 1;
+        self.output = None;
 
-        let parsed_command = self.parse_command(command);
+        let parsed_command = self.parse_command(raw_command.clone());
         let command = parsed_command.0;
         let parameter1 = parsed_command.1;
         let parameter2 = parsed_command.2;
 
-        println!("{} - {} {} {}", self.index, command, parameter1, parameter2);
+        //println!("{} - {} {} {}", self.index, command, parameter1, parameter2);
 
         // Compute the instruction
-        let new_value: i64 = match command.as_ref() {
+        let new_value: Option<i64> = match command.as_ref() {
             "snd" => {
-                let current_value: i64 = self.data_registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone();
+                let parts = raw_command.split_whitespace().collect::<Vec<&str>>();
+                let current_value: i64 = match parts.get(1) {
+                    Some(index) => {
+                        
+                        match index.parse() {
+                            Ok(value) => value,
+                            Err(error) => {
+                                // Lookup the value.
+                                match self.registers.get(&index.to_string()) {
+                                    Some(x) => *x,
+                                    None => 0_i64
+                                }
+                            }
+                        }
+                    },
+                    None => 0_i64
+                };
 
                 // Set the queue
-                new_program.send_registers.insert(parameter1.clone(), current_value);
+                self.output = Some(current_value);
+
+                self.sent_sounds += 1;
 
                 // Return without doing anything.
-                return
+                None
             },
-            "set" => parameter2,
-            "add" => self.data_registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone() + parameter2,
-            "mul" => self.data_registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone() * parameter2,
-            "mod" => self.data_registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone() % parameter2,
+            "set" => Some(parameter2),
+            "add" => Some(self.registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone() + parameter2),
+            "mul" => Some(self.registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone() * parameter2),
+            "mod" => Some(self.registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone() % parameter2),
             "rcv" => {
-                // Load the next value in the receive buffers. If it is not 0, 
+                // Load the next value in the input queue. If it is not 0, 
                 // set it in the appropriate register. Return otherwise.
-                let register_of_interest = self.receive_registers.get(&parameter1.to_string());
-                let (receive_value, remaining_values) = register_of_interest.split_first().unwrap();
+                if self.input_queue.len() > 0 {
+                    let receive_value: i64 = self.input_queue.pop().unwrap();
 
-                if receive_value > 0 {
-                    if recovered_sounds == 0 {
+                    if self.recovered_sounds == 0 {
                         println!("The first recovered sound is {:?}", receive_value);
-                        recovered_sounds += 1;
+                        self.recovered_sounds += 1;
                     }
-                    receive_value
+                    Some(receive_value)
+
                 } else {
-                    return
+                    self.active = false;
+                    None
                 }
+                
             },
             "jgz" => {
-                let current_value: i64 = self.data_registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone();
+                let current_value: i64 = self.registers.get(&parameter1.to_string()).unwrap_or(&0_i64).clone();
                 if current_value > 0 {
                     skip = parameter2;
                 }
-                current_value
+                Some(current_value)
             },
             _ => panic!("Unknown instruction found!")
         };
-        self.data_registers.insert(parameter1, new_value);
 
-        new_program.index =  (self.index as i64 + skip) as usize;
+        match new_value {
+            Some(value) => {self.registers.insert(parameter1, value);},
+            None => (),
+        }
+        
+
+        self.index =  (self.index as i64 + skip) as usize;
     }
 }
 
@@ -112,7 +146,10 @@ fn main() {
     let file = BufReader::new(&f);
 
     let mut program1 = Program::new();
+    program1.registers.insert(String::from("p"), 0);
+
     let mut program2 = Program::new();
+    program2.registers.insert(String::from("p"), 1);
 
     let mut registers: HashMap<String, i64> = HashMap::new();
     
@@ -125,22 +162,40 @@ fn main() {
        
         commands.push(command_string.clone());
     }
-
-    let mut index = 0;
     loop {
-
+        let mut terminate = false;
         for program in 0..2 {
-
             let other_program: usize;
-
-            let mut skip = 1;
-            let command_string = commands.get(index).expect("Index doesn't exist. Terminating");
+            
             if program == 0 {
-                program1 = program1.process_command(command_string);
+                let command_string = commands.get(program1.index).expect("Index doesn't exist. Terminating");
+                program1.process_command(command_string.clone());
+                match program1.output {
+                    Some(value) => {program2.input_queue.insert(0_usize, value);},
+                    None => {}
+                }
+
+                if !program1.active {
+                    terminate = true;
+                    break;
+                }
 
             } else {
-                program2 = program1.process_command(command_string);
+                let command_string = commands.get(program2.index).expect("Index doesn't exist. Terminating");
+                program2.process_command(command_string.clone());
+                match program2.output {
+                    Some(value) => {program1.input_queue.insert(0_usize, value);},
+                    None => {}
+                }
             }
         }
+
+        println!("{:?}", program2.input_queue.len());
+
+        if terminate {
+            println!("terminating");
+            break;
+        }
     }
+    println!("Program 1 has sent a value {} times.", program2.sent_sounds);
 }
